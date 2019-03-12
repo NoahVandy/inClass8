0b15d5ef231bf91e36b0e7670fc0f86c6e75bbec
Noah Vandervelden <NVandervel@my.gcu.edu>
2019-03-12 13:51:02 -0700

    Everything

--- inClass8/Program.cs
+++ inClass8/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//This is my own work
+//Noah Vandervelden
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,33 @@ namespace inClass8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number of grams of fat eaten");
+
+            int fatCals = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of grams of carbs eaten");
+
+            int carbCals = int.Parse(Console.ReadLine());
+
+            fatCals = fatCalories(fatCals);
+            carbCals = carbCalories(carbCals);
+
+            Console.WriteLine("number of calories made up from fat: " + fatCals);
+            Console.WriteLine("number of calories made up from carbs: " + carbCals);
+
+
+        }
+
+        static int fatCalories(int fatCals)
+        {
+            fatCals = fatCals * 9;
+            return fatCals;
+        }
+
+        static int carbCalories(int carbCals)
+        {
+            carbCals = carbCals * 4;
+            return carbCals;
         }
     }
 }
